41587c6dd8029266d40b4d464a31fbe0f2aca167
saseungg <yeon14766@gmail.com>
2023-10-28 23:30:39 +0900

    chore: sitemap 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,8 +96,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://your-blog.netlify.app',
-        sitemap: 'https://your-blog.netlify.app/sitemap.xml',
+        host: 'https://saseungg.github.io',
+        sitemap: 'https://saseungg.github.io/sitemap.xml',
         policy: [
           {
             userAgent: '*',
