d0fb609a61ff5a1dfaae0bb519a6ed811fac1bb8
saseungg <yeon14766@gmail.com>
2023-11-01 15:11:49 +0900

    design: 포인트 컬러 수정
    - category 백그라운드
    - author tag 컬러
    - switch color

--- src/components/theme-switch/index.jsx
+++ src/components/theme-switch/index.jsx
@@ -52,7 +52,7 @@ export const ThemeSwitch = () => {
           width={48}
           checkedIcon={<div className="icon checkedIcon">D</div>}
           uncheckedIcon={<div className="icon uncheckedIcon">L</div>}
-          offColor={'#e6e6e6'}
+          offColor={'#ecf0f2'}
           offHandleColor={'#fff'}
           onColor={'#999'}
           onHandleColor={'#282c35'}
