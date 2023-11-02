b4fe239540af7b694d83b48ac028d45ce3348d6d
saseungg <yeon14766@gmail.com>
2023-11-02 20:58:34 +0900

    design: thumbnail 카테고리, 날짜 추가
    - 카테고리, 날짜 추가
    - light, dark 테마에 따른 색상 변환

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { TARGET_CLASS } from '../../utils/visible'
-
+import { CategoryIcon } from './category-icon'
+import { DateIcon } from './date-icon'
 import './index.scss'
 
 export const ThumbnailItem = ({ node }) => (
   <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
-    <div key={node.fields.slug} className='thumbnail-text'>
+    <div key={node.fields.slug} className="thumbnail-text">
       <h3>{node.frontmatter.title || node.fields.slug}</h3>
       <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+
+      <div className="thumbnail-subinfo">
+        <div className="subinfo-box">
+          <DateIcon />
+          <span>{node.frontmatter.date}</span>
+        </div>
+        <div className="subinfo-box">
+          <CategoryIcon />
+          <span>{node.frontmatter.category}</span>
+        </div>
+      </div>
     </div>
   </Link>
 )
