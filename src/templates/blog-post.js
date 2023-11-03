0188c6a52a251c60a0314b465ddf1a2b7ab23a22
saseungg <yeon14766@gmail.com>
2023-11-03 17:35:20 +0900

    design: hr 제거

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import { graphql } from 'gatsby'
 
-import * as Elements from '../components/elements'
 import { Layout } from '../layout'
 import { Head } from '../components/head'
 import { PostTitle } from '../components/post-title'
@@ -35,7 +34,6 @@ export default ({ data, pageContext, location }) => {
       <PostTitle title={postTitle} />
       <PostDate date={date} />
       <PostContainer html={post.html} />
-      <Elements.Hr />
       <Bio />
       <PostNavigator pageContext={pageContext} />
       {!!disqusShortName && (
