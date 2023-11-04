12397e324c90d66bd7dd364b382d16f280482008
saseungg <yeon14766@gmail.com>
2023-11-04 16:49:42 +0900

    setting: utterances 레포수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -11,7 +11,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'saseungg/blog', // Your repository for archive comment
+    utterances: 'saseungg/saseungg.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
