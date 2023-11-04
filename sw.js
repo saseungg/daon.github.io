d6dc9f813338cad9dce5c8d3f2220473b9ace39f
saseungg <saseungg@users.noreply.github.com>
2023-11-04 15:56:42 +0000

    Automated deployment: Sat Nov  4 15:56:42 UTC 2023 5406ccb58eb47e0ae925ef0d0e809ede7790ae24

--- sw.js
+++ sw.js
@@ -26,10 +26,10 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-41181befea4c227a6899.js"
+    "url": "webpack-runtime-a65ecb658c277419ff20.js"
   },
   {
-    "url": "styles.43408eb6852066280ba8.css"
+    "url": "styles.5c8e6b8e7de8fcf2c8d9.css"
   },
   {
     "url": "styles-29147cbc04bbc833f6a0.js"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8ce4d5e4e97543f1f944257c25fffbae"
+    "revision": "9cda4aeb109c1e079f33e1f66222926d"
   },
   {
     "url": "polyfill-7d3cebe43af022971b58.js"
