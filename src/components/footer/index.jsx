4261418b9ee14e1e874d3232a463231ef5b5520b
saseungg <yeon14766@gmail.com>
2023-11-05 15:41:24 +0900

    feat: footer 수정

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,9 +4,6 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/saseungg">Daon</a>, Built with{' '}
-    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
-      Gatsby-starter-bee
-    </a>
+    © 2023 Daon Powered by Gatsby
   </footer>
 )
