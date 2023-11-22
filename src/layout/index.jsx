cf17501956ea3c95f45555f7ef8f3734b9ced1f7
saseungg <yeon14766@gmail.com>
2023-11-22 18:04:29 +0900

    layout width, header border 수정

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -16,7 +16,7 @@ export const Layout = ({ location, title, children }) => {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(27),
+          maxWidth: rhythm(29),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
